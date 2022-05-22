41bc4331a189bd1f271c21c8a96d4612ffb73d3c
Andrew Shchyolok <andrew.shchyolok@strevus.com>
2022-05-22 22:49:39 +0200

    move new files into separate folder and comment for now (due to variables names duplication)

---
+++ modules/points/1.2/axis.js
@@ -0,0 +1,345 @@
+﻿// const afrikaKorps = {
+//     "4x Panzer IV (short 7.5cm)": 19,
+//     "3x Panzer IV (short 7.5cm)": 14,
+//     "2x Panzer IV (short 7.5cm)": 9,
+//     "1x Panzer IV (short 7.5cm)": 4,
+//
+//     "5x Panzer III (7.5cm)": 35,
+//     "4x Panzer III (7.5cm)": 28,
+//     "3x Panzer III (7.5cm)": 21,
+//     "2x Panzer III (7.5cm)": 14,
+//     "1x Panzer III (7.5cm)": 7,
+//
+//     "5x Panzer III (uparmoured)": 35,
+//     "4x Panzer III (uparmoured)": 28,
+//     "3x Panzer III (uparmoured)": 21,
+//     "2x Panzer III (uparmoured)": 14,
+//     "1x Panzer III (uparmoured)": 7,
+//
+//     "5x Panzer II (2cm)": 9,
+//     "4x Panzer II (2cm)": 7,
+//     "3x Panzer II (2cm)": 5,
+//
+//     "2x Sd Kfz 222 (2cm)<br>1x Sd Kfz 221 (MG)": 3,
+//     "2x Sd Kfz 222 (2cm)<br>2x Sd Kfz 221 (MG)": 4,
+//
+//     "3x Diana (7.62cm)": 12,
+//     "2x Diana (7.62cm)": 8,
+//
+//     "3x 7.62cm gun": 13,
+//     "2x 7.62cm gun": 9,
+//
+//     "4x Sd Kfz 10/4 (2cm)": 4,
+//     "3x Sd Kfz 10/4 (2cm)": 3,
+//     "2x Sd Kfz 10/4 (2cm)": 2,
+//
+//     "4x Marder (7.62cm)": 22,
+//     "3x Marder (7.62cm)": 16,
+//     "2x Marder (7.62cm)": 10,
+//
+//     "3x 5cm guns": 9,
+//     "2x 5cm guns": 6,
+//
+//     "4x 10.5cm howitzer": 12,
+//     "2x 10.5cm howitzer": 6
+// }
+//
+// const ironCross = {
+//     "4x Panzer IV (short 7.5cm)": 19,
+//     "3x Panzer IV (short 7.5cm)": 14,
+//     "2x Panzer IV (short 7.5cm)": 9,
+//     "1x Panzer IV (short 7.5cm)": 4,
+//
+//     "5x Panzer III (7.5cm)": 35,
+//     "4x Panzer III (7.5cm)": 28,
+//     "3x Panzer III (7.5cm)": 21,
+//     "2x Panzer III (7.5cm)": 14,
+//     "1x Panzer III (7.5cm)": 7,
+//
+//     "5x Panzer III (uparmoured)": 35,
+//     "4x Panzer III (uparmoured)": 28,
+//     "3x Panzer III (uparmoured)": 21,
+//     "2x Panzer III (uparmoured)": 14,
+//     "1x Panzer III (uparmoured)": 7,
+//
+//     "3x StuG (short 7.5cm)": 12,
+//     "2x StuG (short 7.5cm)": 8,
+//     "4x StuG (long 7.5cm)": 28,
+//     "3x StuG (long 7.5cm)": 21,
+//     "2x StuG (long 7.5cm)": 13,
+//
+//     "4x 5cm gun": 12,
+//     "3x 5cm gun": 9,
+//     "2x 5cm gun": 6,
+//
+//     "4x 3.7cm gun": 7,
+//     "3x 3.7cm gun": 5,
+//     "2x 3.7cm gun": 3,
+//
+//     "4x 3.7cm (Stielgranate) gun": 9,
+//     "3x 3.7cm (Stielgranate) gun": 7,
+//     "2x 3.7cm (Stielgranate) gun": 4,
+//
+//     "2x Sd Kfz 222 (2cm)<br>1x Sd Kfz 221 (MG)": 3,
+//     "2x Sd Kfz 222 (2cm)<br>2x Sd Kfz 221 (MG)": 4,
+//
+//     "4x Marder II (7.5cm)": 22,
+//     "3x Marder II (7.5cm)": 16,
+//     "2x Marder II (7.5cm)": 10,
+//
+//     "3x Sd Kfz 7/1 (2cm quad)": 4,
+//     "2x Sd Kfz 7/1 (2cm quad)": 3,
+//
+//     "4x 7.5cm infantry gun": 11,
+//     "2x 7.5cm infantry gun": 5,
+//
+//     "2x 15cm infantry gun": 6,
+//
+//     "4x 10.5cm howitzer": 12,
+//     "2x 10.5cm howitzer": 6,
+//
+//     "4x Sd Kfz 10/4 (2cm)": 4,
+//     "3x Sd Kfz 10/4 (2cm)": 3,
+//     "2x Sd Kfz 10/4 (2cm)": 2
+//
+// }
+//
+// const ghostPanzers = {
+//     "5x Panzer IV (late 7.5cm)": 45,
+//     "4x Panzer IV (late 7.5cm)": 36,
+//     "3x Panzer IV (late 7.5cm)": 27,
+//     "2x Panzer IV (late 7.5cm)": 18,
+//     "1x Panzer IV (late 7.5cm)": 9,
+//
+//     "1x Panzer IV (late 7.5cm)<br>1x Panzer III (late 5cm)": 16,
+//
+//     "5x Panzer III (late 5cm)": 36,
+//     "4x Panzer III (late 5cm)": 29,
+//     "3x Panzer III (late 5cm)": 22,
+//     "2x Panzer III (late 5cm)": 14,
+//     "1x Panzer III (late 5cm)": 7,
+//
+//     "5x Panther (7.5cm)": 65,
+//     "4x Panther (7.5cm)": 52,
+//     "3x Panther (7.5cm)": 39,
+//     "2x Panther (7.5cm)": 26,
+//     "1x Panther (7.5cm)": 13,
+//
+//     "4x StuG (late 7.5cm)": 36,
+//     "3x StuG (late 7.5cm)": 27,
+//     "2x StuG (late 7.5cm)": 18,
+//
+//     "1x StuG (late 7.5cm)": 9,
+//     "2x StuG (late 7.5cm)<br>1x StuH (10.5cm)": 27,
+//
+//     "7x Flammpanzer III": 29,
+//     "6x Flammpanzer III": 25,
+//     "5x Flammpanzer III": 21,
+//     "4x Flammpanzer III": 17,
+//     "3x Flammpanzer III": 13,
+//     "2x Flammpanzer III": 9,
+//
+//     "4x Brummbär (15cm)": 24,
+//     "3x Brummbär (15cm)": 18,
+//     "2x Brummbär (15cm)": 12,
+//     "2x Brummbär": 12,
+//     "1x Brummbär": 6,
+//
+//     "4x Marder (7.6cm)": 22,
+//     "3x Marder (7.62cm)": 16,
+//     "2x Marder (7.62cm)": 10,
+//
+//     "2x Sd Kfz 222 (2cm)<br>1x Sd Kfz 221 (MG)": 3,
+//
+//     "4x Sd Kfz 10/4 (2cm)": 4,
+//     "3x Sd Kfz 10/4 (2cm)": 3,
+//     "2x Sd Kfz 10/4 (2cm)": 2,
+//
+//     "4x 5cm gun": 12,
+//     "3x 5cm gun": 9,
+//     "2x 5cm gun": 6,
+//
+//     "3x Sd Kfz 7/1 (2cm quad)": 4,
+//     "2x Sd Kfz 7/1 (2cm quad)": 3,
+//
+//     "4x 7.5cm infantry gun": 11,
+//     "2x 7.5cm infantry gun": 5,
+//
+//     "2x 15cm infantry gun": 6,
+//
+//     "4x 10.5cm howitzer": 12,
+//     "2x 10.5cm howitzer": 6,
+//
+// }
+//
+// const avanti = {
+//     "4x Autocannone (20mm)": 4,
+//     "2x Autocannone (20mm)": 2,
+//
+//     "3x Breda MG team<br>1x Breda HMG team<br>1x 20mm anti-tank rifle": 5,
+//     "2x Breda MG team<br>1x Breda HMG team<br>1x 20mm anti-tank rifle": 4,
+//
+//     "5x Breda MG team & Carcano rifle team<br>1x Brixia mortar team<br>1x Flame-thrower team": 12,
+//     "3x Breda MG team & Carcano rifle team<br>1x Brixia mortar team<br>1x Flame-thrower team": 6,
+//
+//     "4x 47mm gun": 6,
+//     "3x 47mm gun": 4,
+//     "2x 47mm gun": 3,
+//
+//     "5x L6/40 (20mm)": 7,
+//     "4x L6/40 (20mm)": 5,
+//     "3x L6/40 (20mm)": 4,
+//
+//     "4x AB41 (20mm)": 4,
+//     "3x AB41 (20mm)": 3,
+//
+//     "4x 100mm howitzer": 10,
+//     "2x 100mm howitzer": 5
+//
+// }
+//
+// const whiteDeath = {
+//     "3x T-34 (76mm)": 21,
+//     "2x T-34 (76mm)": 14,
+//
+//     "3x 81mm mortar": 4,
+//     "2x 81mm mortar": 3,
+//
+//     "2x 3.7cm gun": 4,
+//     "2x 3.7cm (Stielgranate) gun": 5,
+//
+//     "2x 45mm gun": 5,
+//
+//     "4x 3.7cm (Stielgranate) gun": 9,
+//     "3x 3.7cm (Stielgranate) gun": 7,
+//     "2x 3.7cm (Stielgranate) gun": 4,
+//
+//     "3x BT-42 (114mm)": 10,
+//     "2x BT-42 (114mm)": 7,
+//
+//     "4x 105mm howitzer": 10,
+//     "2x 105mm howitzer": 5,
+//
+//     "4x 122mm howitzer": 8,
+//     "2x 122mm howitzer": 4
+//
+// }
+//
+// const hungarianSteel = {
+//     "3x Panzer IV (short 7.5cm)": 18,
+//     "2x Panzer IV (short 7.5cm)": 12,
+//     "1x Panzer IV (short 7.5cm)": 6,
+//
+//     "3x Panzer III (5cm)": 18,
+//     "2x Panzer III (5cm)": 12,
+//     "3x Panzer III (7.5cm)": 19,
+//     "2x Panzer III (7.5cm)": 13,
+//
+//     "4x 40mm gun": 6,
+//     "2x 40mm gun": 3,
+//
+//     "4x 40mm gun with Kerngranate": 8,
+//     "2x 40mm gun with Kerngranate ": 4,
+//
+//     "4x 50mm gun": 9,
+//     "2x 50mm gun": 4,
+//
+//     "4x 81mm mortar": 3,
+//
+//     "4x Short 75mm gun": 8,
+//     "2x Short 75mm gun": 4,
+//
+//     "4x 75mm mountain gun": 8,
+//     "2x 75mm mountain gun": 4,
+//
+//     "3x Csaba": 3,
+//
+//     "4x Marder II (7.5cm)": 23,
+//     "3x Marder II (7.5cm)": 17,
+//     "2x Marder II (7.5cm)": 11
+//
+// }
+//
+// const braveRomania = {
+//     "3x T-3 (7.5cm)": 18,
+//     "1x T-3 (7.5cm)": 6,
+//
+//     "3x T-4 (Long 7.5cm)": 21,
+//     "1x T-4 (Long 7.5cm)": 7,
+//
+//     "3x 50mm gun": 9,
+//     "2x 50mm gun": 6,
+//
+//     "2x AB (2cm)<br>1x AB (MG)": 3
+//     }
+//
+// const namedGerman = {
+//     "Armoured Car Company": {
+//         "1x Sd Kfz 221 (MG)" : 3,
+//         "1x Sd Kfz 221 (MG) armoured car from (MG223)": 3,
+//         "1x Luchs (2cm) tank from (MG258)": 4
+//     },
+//
+//     "Captured 6 pdr Anti-tank (with 3 guns)" : {
+//         "Command Card Captured 6 pdr Anti-tank (with 3 guns)": -1
+//     },
+//     "Captured 6 pdr Anti-tank (with 2 guns)" : {
+//         "Command Card Captured 6 pdr Anti-tank (with 2 guns)": -1
+//     },
+//     "Armoured FlaK Half-tracks": {
+//         "Armoured FlaK Half-tracks Command Card": 2
+//     }
+// }
+//
+// const namedItalian = {
+//
+//     "Tactical Brilliance": {
+//         "Command Card Tactical Brilliance": 3
+//     },
+//
+//     "Captured 25 pdr Artillery": {
+//         "Command Card Captured 25 pdr Artillery": 2
+//     },
+//
+//     "AB41 Armoured Car Company HQ": {
+//         "2x AB41 (20mm) from (MI113)": 3
+//     },
+//
+//     "L6/40 Scout Tank Company HQ": {
+//         "4x L6/40 (20mm)": 7,
+//         "3x L6/40 (20mm)": 6,
+//         "2x L6/40 (20mm)": 5
+//     },
+// }
+//
+// const italianNamedFormationOverrides = {
+//     "L6/40 Light Tank Company Command Card" : {
+//         "L6/40 Light Tank Company HQ": {
+//             "4x L6/40 (20mm)": 8,
+//             "3x L6/40 (20mm)": 6,
+//             "2x L6/40 (20mm)": 4
+//         },
+//
+//         "L6/40 Light Tank Platoon": {
+//             "5x L6/40 (20mm)": 10,
+//             "4x L6/40 (20mm)": 8,
+//             "3x L6/40 (20mm)": 6,
+//         }
+//     }
+// }
+//
+// const italianNumberedFormationOverrides = {
+//     "156": {
+//         "L6/40 Light Tank Company HQ": {
+//             "4x L6/40 (20mm)": 8,
+//             "3x L6/40 (20mm)": 6,
+//             "2x L6/40 (20mm)": 4
+//         },
+//
+//         "L6/40 Light Tank Platoon": {
+//             "5x L6/40 (20mm)": 10,
+//             "4x L6/40 (20mm)": 8,
+//             "3x L6/40 (20mm)": 6,
+//         }
+//     }
+// }
---
+++ modules/points/1.2/soviets.js
@@ -0,0 +1,175 @@
+// const enemyAtTheGates = {
+//     "10x T-34 (76mm)": 40,
+//     "9x T-34 (76mm)": 36,
+//     "8x T-34 (76mm)": 32,
+//     "7x T-34 (76mm)": 28,
+//     "6x T-34 (76mm)": 24,
+//     "5x T-34 (76mm)": 20,
+//     "4x T-34 (76mm)": 16,
+//     "3x T-34 (76mm)": 12,
+//     "1x T-34 (76mm)": 4,
+//
+//     "10x OT-34 (76mm)": 40,
+//     "9x OT-34 (76mm)": 36,
+//     "8x OT-34 (76mm)": 32,
+//     "7x OT-34 (76mm)": 28,
+//     "6x OT-34 (76mm)": 24,
+//     "5x OT-34 (76mm)": 20,
+//     "4x OT-34 (76mm)": 16,
+//     "3x OT-34 (76mm)": 12,
+//
+//     "3x BA-10 (45mm)": 3,
+//
+//     "28x M1891 rifle team<br>1x Komissar team": 17,
+//     "19x M1891 rifle team<br>1x Komissar team": 11,
+//
+//     "28x DP MG &amp; M1891 rifle team<br>1 Komissar team": 18,
+//     "19 DP MG &amp; M1891 rifle team<br>1 Komissar team": 12,
+//
+//     "22x PPSh SMG team<br>1x Komissar team": 17,
+//     "15x PPSh SMG team<br>1x Komissar team": 12,
+//
+//     "5x KV-1s (76mm)": 33,
+//     "4x KV-1s (76mm)": 28,
+//     "3x KV-1s (76mm)": 19,
+//
+//     "7x PPSh SMG team<br>2x Maksim HMG team<br>1x PTRD AT rifle team<br>1x 50mm mortar team": 16,
+//     "5x PPSh SMG team<br>2x Maksim HMG team<br>1x PTRD AT rifle team<br>1x 50mm mortar team": 13,
+//
+//     "4x Short 45mm gun": 8,
+//     "2x Short 45mm gun": 4,
+//
+//     "4x Long 45mm gun": 11,
+//     "2x Long 45mm gun": 5,
+//
+//     "4x 76mm gun": 13,
+//     "2x 76mm gun": 7
+//
+// }
+//
+// const redBanner = {
+//     "10x T-34 (76mm)": 45,
+//     "9x T-34 (76mm)": 40,
+//     "8x T-34 (76mm)": 35,
+//     "7x T-34 (76mm)": 30,
+//     "6x T-34 (76mm)": 25,
+//     "5x T-34 (76mm)": 21,
+//     "4x T-34 (76mm)": 17,
+//     "3x T-34 (76mm)": 13,
+//     "1x T-34 (76mm)": 4,
+//
+//     "1x T-34 (76mm)": 6,
+//     "3x T-34 (76mm)": 17,
+//
+//     "5x KV-1s (76mm)": 43,
+//     "4x KV-1s (76mm)": 34,
+//     "3x KV-1s (76mm)": 25,
+//     "2x KV-1s (76mm)": 16,
+//     "1x KV-1s (76mm)": 8,
+//
+//     "4x SU-85 (85mm)": 29,
+//     "3x SU-85 (85mm)": 22,
+//     "2x SU-85 (85mm)": 15,
+//
+//     "4x SU-122 (122mm)": 18,
+//     "3x SU-122 (122mm)": 14,
+//     "2x SU-122 (122mm)": 9,
+//
+//     "2x SU-152 (152mm)": 14,
+//
+//     "10x OT-34 (76mm)": 40,
+//     "9x OT-34 (76mm)": 36,
+//     "8x OT-34 (76mm)": 32,
+//     "7x OT-34 (76mm)": 28,
+//     "6x OT-34 (76mm)": 24,
+//     "5x OT-34 (76mm)": 20,
+//     "4x OT-34 (76mm)": 16,
+//     "3x OT-34 (76mm)": 12,
+//
+//     "28x M1891 rifle team<br>1x Komissar team": 22,
+//     "19x M1891 rifle team<br>1x Komissar team": 15,
+//
+//     "22x PPSh SMG team<br>1x Komissar team": 17,
+//     "15x PPSh SMG team<br>1x Komissar team": 12,
+//
+//     "4x Short 45mm gun": 8,
+//     "2x Short 45mm gun": 4,
+//
+//     "4x Long 45mm gun": 11,
+//     "2x Long 45mm gun": 5,
+//
+//     "4x 76mm gun": 13,
+//     "2x 76mm gun": 7,
+//
+//     "6x PPSh SMG team<br>3x M3 Scout transport (.50 cal MG) (MS156)": 7,
+//     "4x PPSh SMG team<br>2x M3 Scout transport (.50 cal MG) (MS156)": 5,
+//     "6x PPSh SMG team<br>3x Sd Kfz 251 transport (MG) (MS162)": 7,
+//     "4x PPSh SMG team<br>2x Sd Kfz 251 transport (MG) (MS162)": 5,
+//
+//     "4x Recce DP MG taem<br>1x Flame-thrower team": 6,
+//     "4x Recce DP MG team": 4
+//
+// }
+//
+// const namedSoviet = {
+//
+//     "T-34 Tank Battalion HQ" : {
+//         "1x T-34 (76mm)" : 4
+//     },
+//
+//     "T-34 Tank Company" : {
+//         "10x T-34 (76mm)": 45,
+//         "9x T-34 (76mm)": 40,
+//         "8x T-34 (76mm)": 35,
+//         "7x T-34 (76mm)": 30,
+//         "6x T-34 (76mm)": 25,
+//         "5x T-34 (76mm)": 21,
+//         "4x T-34 (76mm)": 17,
+//         "3x T-34 (76mm)": 13
+//     },
+//
+//     "T-34 Hero Tank Battalion HQ" : {
+//         "1x T-34 (76mm)" : 6
+//     },
+//
+//     "T-34 Hero Tank Company" : {
+//         "3x T-34 (76mm)": 17
+//     },
+//
+//     "Rifle Company" : {
+//         "28x DP MG and M1891 rifle team<br>1x Komissar team": 18,
+//         "19x DP MG and M1891 rifle team<br>1x Komissar team": 12
+//     },
+//
+//     "Motor Rifle Company" : {
+//         "28x DP MG &amp; M1891 rifle team<br>1x Komissar team": 22,
+//         "19x DP MG &amp; M1891 rifle team<br>1x Komissar team": 15
+//     },
+//
+//     "KV-1s Guards Heavy Tank Company": {
+//         "5x KV-1s (76mm)": 43,
+//         "4x KV-1s (76mm)": 34,
+//         "3x KV-1s (76mm)": 25,
+//         "2x KV-1s (76mm)": 16
+//     },
+//
+//     "KV-1s Guards Heavy Tank Regiment HQ": {
+//         "1x KV-1s (76mm)": 8
+//     },
+//
+//     "Reconnaissance by Combat": {
+//         "Command Card Reconnaissance by Combat": 3
+//     },
+//
+//     "Reconnaissance By Combat": {
+//         "Command Card Reconnaissance by Combat": 3
+//     },
+//
+//     "BA-10 Armoured Car Company": {
+//         "1x BA-10 (45mm)": 3
+//     },
+//
+//     "SU-152 Medium SP Regiment" : {
+//         "1x KV-1s (76mm)": 8
+//     }
+// }
